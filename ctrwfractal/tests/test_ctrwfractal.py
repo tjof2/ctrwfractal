54e2a58b827e4fb75f840b5b579c8332b4e693dc
Tom Furnival <tjof2@cam.ac.uk>
2020-06-09 16:28:42 +0100

    Clean up tests

--- ctrwfractal/tests/test_ctrwfractal.py
+++ ctrwfractal/tests/test_ctrwfractal.py
@@ -15,6 +15,8 @@
 # You should have received a copy of the GNU General Public License
 # along with ctrwfractal.  If not, see <http://www.gnu.org/licenses/>.
 
+import hashlib
+
 import numpy as np
 import pandas as pd
 import pytest
@@ -22,39 +24,61 @@ import pytest
 from ctrwfractal import CTRWfractal
 
 
+def _hash_ndarray(arr, n_char=-1):
+    """Simple function to hash a np.ndarray object."""
+    return hashlib.sha256(arr.data.tobytes()).hexdigest()[:n_char]
+
+
 class TestSquare:
     def setup_method(self, method):
         self.seed = 123
         self.grid_size = 32
 
-    @pytest.mark.parametrize("threshold", [None, 0.55, 0.65])
-    def test_square_no_walks(self, threshold):
-        est = CTRWfractal(
+    @pytest.mark.parametrize(
+        "threshold, expected_threshold, expected_clusters_hash",
+        [
+            (None, 0.592746, "7f5e0c80"),
+            (0.55, 0.55, "37cd4e13"),
+            (0.65, 0.65, "cc01a103"),
+        ],
+    )
+    def test_square_no_walks(
+        self, threshold, expected_threshold, expected_clusters_hash
+    ):
+        s = CTRWfractal(
             grid_size=self.grid_size,
             lattice_type="square",
             threshold=threshold,
             random_seed=self.seed,
         )
-        est.run()
+        s.run()
 
-        for attr in ["lattice_", "clusters_", "analysis_", "walks_"]:
-            assert hasattr(est, attr)
+        for attr in [
+            "lattice_",
+            "clusters_",
+            "analysis_",
+            "walks_",
+            "occupied_fraction_",
+        ]:
+            assert hasattr(s, attr)
 
-        assert est.clusters_.shape == (self.grid_size * self.grid_size,)
-        assert est.walks_ is None
-        assert est.analysis_ is None
+        assert s.clusters_.shape == (self.grid_size * self.grid_size,)
+        assert s.walks_ is None
+        assert s.analysis_ is None
 
         # Check occupied fraction
-        expected_threshold = 0.592746 if threshold is None else threshold
         np.testing.assert_allclose(
-            expected_threshold, est.occupied_fraction_, atol=5e-3,
+            expected_threshold, s.occupied_fraction_, atol=5e-3,
         )
+
+        # Check clusters hash (first 8 characters)
+        assert _hash_ndarray(s.clusters_, 8) == expected_clusters_hash
 
     @pytest.mark.parametrize("walk_type", ["all", "largest"])
     @pytest.mark.parametrize("n_walks", [1, 2])
     @pytest.mark.parametrize("n_steps", [10, 25])
     def test_square_with_walks(self, walk_type, n_walks, n_steps):
-        est = CTRWfractal(
+        s = CTRWfractal(
             grid_size=self.grid_size,
             lattice_type="square",
             walk_type=walk_type,
@@ -62,14 +86,13 @@ class TestSquare:
             n_steps=n_steps,
             random_seed=self.seed,
         )
-        est.run()
+        s.run()
 
-        for attr in ["lattice_", "clusters_", "analysis_", "walks_"]:
-            assert hasattr(est, attr)
+        assert isinstance(s.walks_, np.ndarray)
+        assert isinstance(s.analysis_, pd.DataFrame)
 
-        assert est.clusters_.shape == (self.grid_size * self.grid_size,)
-        assert isinstance(est.walks_, np.ndarray)
-        assert isinstance(est.analysis_, pd.DataFrame)
+        assert s.walks_.shape == (n_walks, n_steps, 2)
+        assert s.analysis_.shape == (n_steps - 1, n_walks + 3)
 
 
 class TestHoneycomb:
@@ -77,34 +100,51 @@ class TestHoneycomb:
         self.seed = 123
         self.grid_size = 32
 
-    @pytest.mark.parametrize("threshold", [None, 0.65, 0.75])
-    def test_honeycomb_no_walks(self, threshold):
-        est = CTRWfractal(
+    @pytest.mark.parametrize(
+        "threshold, expected_threshold, expected_clusters_hash",
+        [
+            (None, 0.697040230, "f4d56b22"),
+            (0.65, 0.65, "8d0528ba"),
+            (0.75, 0.75, "35bcc465"),
+        ],
+    )
+    def test_honeycomb_no_walks(
+        self, threshold, expected_threshold, expected_clusters_hash
+    ):
+        s = CTRWfractal(
             grid_size=self.grid_size,
             lattice_type="honeycomb",
             threshold=threshold,
             random_seed=self.seed,
         )
-        est.run()
+        s.run()
 
-        for attr in ["lattice_", "clusters_", "analysis_", "walks_"]:
-            assert hasattr(est, attr)
+        for attr in [
+            "lattice_",
+            "clusters_",
+            "analysis_",
+            "walks_",
+            "occupied_fraction_",
+        ]:
+            assert hasattr(s, attr)
 
-        assert est.clusters_.shape == (4 * self.grid_size * self.grid_size,)
-        assert est.walks_ is None
-        assert est.analysis_ is None
+        assert s.clusters_.shape == (4 * self.grid_size * self.grid_size,)
+        assert s.walks_ is None
+        assert s.analysis_ is None
 
         # Check occupied fraction
-        expected_threshold = 0.697040230 if threshold is None else threshold
         np.testing.assert_allclose(
-            expected_threshold, est.occupied_fraction_, atol=5e-3,
+            expected_threshold, s.occupied_fraction_, atol=5e-3,
         )
+
+        # Check clusters hash (first 8 characters)
+        assert _hash_ndarray(s.clusters_, 8) == expected_clusters_hash
 
     @pytest.mark.parametrize("walk_type", ["all", "largest"])
     @pytest.mark.parametrize("n_walks", [1, 2])
     @pytest.mark.parametrize("n_steps", [10, 25])
     def test_honeycomb_with_walks(self, walk_type, n_walks, n_steps):
-        est = CTRWfractal(
+        s = CTRWfractal(
             grid_size=self.grid_size,
             lattice_type="honeycomb",
             walk_type=walk_type,
@@ -112,14 +152,13 @@ class TestHoneycomb:
             n_steps=n_steps,
             random_seed=self.seed,
         )
-        est.run()
+        s.run()
 
-        for attr in ["lattice_", "clusters_", "analysis_", "walks_"]:
-            assert hasattr(est, attr)
+        assert isinstance(s.walks_, np.ndarray)
+        assert isinstance(s.analysis_, pd.DataFrame)
 
-        assert est.clusters_.shape == (4 * self.grid_size * self.grid_size,)
-        assert isinstance(est.walks_, np.ndarray)
-        assert isinstance(est.analysis_, pd.DataFrame)
+        assert s.walks_.shape == (n_walks, n_steps, 2)
+        assert s.analysis_.shape == (n_steps - 1, n_walks + 3)
 
 
 class TestErrors:
@@ -128,39 +167,39 @@ class TestErrors:
         self.grid_size = 32
 
     def test_lattice_type_error(self):
-        est = CTRWfractal(grid_size=self.grid_size, lattice_type=None)
+        s = CTRWfractal(grid_size=self.grid_size, lattice_type=None)
         with pytest.raises(ValueError, match="Invalid lattice_type parameter"):
-            est.run()
+            s.run()
 
-        est = CTRWfractal(grid_size=self.grid_size, lattice_type="triangle")
+        s = CTRWfractal(grid_size=self.grid_size, lattice_type="triangle")
         with pytest.raises(ValueError, match="Invalid lattice_type parameter"):
-            est.run()
+            s.run()
 
     def test_walk_type_error(self):
-        est = CTRWfractal(grid_size=self.grid_size, walk_type=None)
+        s = CTRWfractal(grid_size=self.grid_size, walk_type=None)
         with pytest.raises(ValueError, match="Invalid walk_type parameter"):
-            est.run()
+            s.run()
 
-        est = CTRWfractal(grid_size=self.grid_size, walk_type="smallest")
+        s = CTRWfractal(grid_size=self.grid_size, walk_type="smallest")
         with pytest.raises(ValueError, match="Invalid walk_type parameter"):
-            est.run()
+            s.run()
 
     def test_threshold_error(self):
-        est = CTRWfractal(grid_size=self.grid_size, threshold=-0.2)
+        s = CTRWfractal(grid_size=self.grid_size, threshold=-0.2)
         with pytest.raises(ValueError, match="Invalid threshold parameter"):
-            est.run()
+            s.run()
 
     def test_beta_error(self):
-        est = CTRWfractal(grid_size=self.grid_size, beta=-0.2)
+        s = CTRWfractal(grid_size=self.grid_size, beta=-0.2)
         with pytest.raises(ValueError, match="Invalid beta parameter"):
-            est.run()
+            s.run()
 
     def test_tau0_error(self):
-        est = CTRWfractal(grid_size=self.grid_size, tau0=-0.2)
+        s = CTRWfractal(grid_size=self.grid_size, tau0=-0.2)
         with pytest.raises(ValueError, match="Invalid tau0 parameter"):
-            est.run()
+            s.run()
 
     def test_noise_error(self):
-        est = CTRWfractal(grid_size=self.grid_size, noise=-0.2)
+        s = CTRWfractal(grid_size=self.grid_size, noise=-0.2)
         with pytest.raises(ValueError, match="Invalid noise parameter"):
-            est.run()
+            s.run()
